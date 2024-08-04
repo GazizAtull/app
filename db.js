6fd9299846dae48e86bab8c1b5e11e08037f1644
GazizAtull <138782569+GazizAtull@users.noreply.github.com>
2024-08-04 22:36:53 +0500

    Update db.js

--- db.js
+++ db.js
@@ -1,20 +1,26 @@
 const { MongoClient } = require('mongodb');
-let dbConnection;
-require("dotenv").config;
-const DATABASE=process.env.DATABASE;
+require('dotenv').config(); 
 
-const connectToDb = (cb) => {
-    MongoClient.connect(DATABASE, { useNewUrlParser: true, useUnifiedTopology: true })
-        .then((client) => {
-            dbConnection = client.db('cryptoWallets'); // Замените на имя вашей базы данных
-            cb(null);
-        })
-        .catch((err) => {
-            console.log(err);
-            cb(err);
-        });
+const DATABASE = process.env.DATABASE;
+
+let dbConnection;
+
+const connectToDb = async () => {
+    try {
+        const client = await MongoClient.connect(DATABASE, { useNewUrlParser: true, useUnifiedTopology: true });
+        dbConnection = client.db('cryptoWallets'); 
+        console.log('Connected to database');
+    } catch (err) {
+        console.error('Error connecting to database:', err);
+        throw err;
+    }
 };
 
-const getDb = () => dbConnection;
+const getDb = () => {
+    if (!dbConnection) {
+        throw new Error('Database not connected');
+    }
+    return dbConnection;
+};
 
 module.exports = { connectToDb, getDb };
